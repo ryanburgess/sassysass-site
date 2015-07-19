f49d9ae4d8ed28dac81697fc9fcfa0ddddac0c02
ryanburgess <ryanb@netflix.com>
2015-07-18 23:34:59 -0700

    add analytics

--- views/default.js
+++ views/default.js
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var analytics = require('./analytics');
 var DefaultLayout = React.createClass({
   render: function() {
     return (
@@ -9,6 +9,7 @@ var DefaultLayout = React.createClass({
           <meta name="description" content={this.props.description}/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
           <link rel="stylesheet" href="/css/style.css"/>
+          <script dangerouslySetInnerHTML={{__html: analytics('UA-63183456-1')}}></script>
         </head>
         <body>
           <div className='container'>
